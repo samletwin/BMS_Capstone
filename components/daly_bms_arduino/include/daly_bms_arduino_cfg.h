32d5dd0608d590be4629a6f22bf66951d442a9b0
samletwin <samletwin@yahoo.ca>
2024-07-25 01:31:27 -0400

    adding daly_bms_uart library

---
+++ components/daly_bms_arduino/include/daly_bms_arduino_cfg.h
@@ -0,0 +1,9 @@
+#ifndef DALY_BMS_ARDUINO_CFG_H
+#define DALY_BMS_ARDUINO_CFG_H
+
+// Add these defines for the UART configuration
+#define DALY_BMS_RX_PIN 5  // Example RX pin
+#define DALY_BMS_TX_PIN 6  // Example TX pin
+#define DALY_BMS_UART_PORT 1  // Example UART port number
+
+#endif /* DALY_BMS_ARDUINO_CFG_H */
---
+++ components/lvgl_example/include/lvgl_example.h
@@ -0,0 +1,14 @@
+#ifndef LVGL_EXAMPLE_H
+#define LVGL_EXAMPLE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void lv_example_menu_5(void);
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
+
+#endif /* LVGL_EXAMPLE_H */
